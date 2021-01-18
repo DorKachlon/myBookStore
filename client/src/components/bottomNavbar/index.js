77f72af725b54577a3243962c97537534c21c75c
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2021-01-18 15:39:17 +0200

    bottom navber for books and categories links

---
+++ client/src/components/bottomNavbar/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./style.css";
+import { Link } from "react-router-dom";
+import MenuBookIcon from "@material-ui/icons/MenuBook";
+import CategoryIcon from "@material-ui/icons/Category";
+import Tooltip from "@material-ui/core/Tooltip";
+
+export default function BottomNavbar() {
+  return (
+    <div>
+      <Link to="/books">
+        <Tooltip title="Books" placement="top">
+          <button className="bottom-navbar-books">
+            <MenuBookIcon style={{ color: "white" }} />
+          </button>
+        </Tooltip>
+      </Link>
+      <Link to="/categories">
+        <Tooltip title="Categories" placement="top">
+          <button className="bottom-navbar-categories">
+            <CategoryIcon style={{ color: "white" }} />
+          </button>
+        </Tooltip>
+      </Link>
+    </div>
+  );
+}
