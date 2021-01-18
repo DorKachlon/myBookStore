d30db140a50712c9fcda35cdf7d521c519cdb691
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2021-01-18 15:41:43 +0200

    books page

--- client/src/components/booksDashboard/index.js
+++ client/src/components/booksDashboard/index.js
@@ -1,21 +1,57 @@
 import React from "react";
 import "./style.css";
-import { useSelector, useDispatch } from "react-redux";
-import { addBook } from "../../actions/books";
+import Chip from "@material-ui/core/Chip";
+import { connect, useSelector } from "react-redux";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
+import { useDispatch } from "react-redux";
+import { removeBook } from "../../actions/books";
 
-export default function BooksDashboard() {
-  const dispatch = useDispatch();
+function BooksDashboard(prop) {
   const books = useSelector((state) => state.books);
-  const clickHandler = () => {
-    // localStorage.setItem("myFirstKey", "myFirstValue");
-    dispatch(addBook("hello"));
+  const deletion = useSelector((state) => state.deletion);
+  const editing = useSelector((state) => state.editing);
+  const dispatch = useDispatch();
+
+  const deleteHandler = (toDelete) => {
+    console.log(toDelete);
+    dispatch(removeBook(toDelete));
   };
   return (
-    <div>
-      <button onClick={clickHandler}>click</button>
-      {books.map((book) => (
-        <h1>{book}</h1>
-      ))}
-    </div>
+    <>
+      <h1 className="books-dashboard-title">Books list</h1>
+      <div className="books-dashboard">
+        {books &&
+          books.map((book, i) => (
+            <div key={i} className="book-card">
+              <h2 className="book-card-name">{book.name}</h2>
+              <div className="book-card-author">{book.author}</div>
+              <div className="book-card-price">{book.price}</div>
+              <Chip label={book.category} />
+              {deletion || editing ? (
+                <div className="book-card-control-panel">
+                  {deletion && (
+                    <button className="book-card-delete" onClick={() => deleteHandler(book)}>
+                      <DeleteIcon style={{ color: "white" }} />
+                    </button>
+                  )}
+                  {editing && (
+                    <button className="book-card-edit">
+                      <EditIcon style={{ color: "white" }} />
+                    </button>
+                  )}
+                </div>
+              ) : (
+                <></>
+              )}
+            </div>
+          ))}
+      </div>
+    </>
   );
 }
+const mapStateToProps = (state) => {
+  return { books: state.books };
+};
+
+export default connect(mapStateToProps, null)(BooksDashboard);
---
+++ client/src/pages/books/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+import BooksDashboard from "../../components/booksDashboard";
+
+export default function Books() {
+  return (
+    <div className="books-page page">
+      <BooksDashboard />
+    </div>
+  );
+}
