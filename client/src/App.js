221e919746a05b01e80196cbe297b06fb3bae590
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2021-01-18 20:38:47 +0200

    categories redux

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/books";
 import AddBook from "./pages/addBook";
 import AddCategory from "./pages/addCategory";
+import Categories from "./pages/categories";
 
 import BottomNavbar from "./components/bottomNavbar";
 import TopNavbar from "./components/topNavbar";
 import { useDispatch } from "react-redux";
 import { fetchBooks } from "./actions/books";
+import { fetchCategories } from "./actions/categories";
 
 function App() {
   const dispatch = useDispatch();
@@ -18,13 +20,20 @@ function App() {
     if (booksFromLocalStorage) {
       dispatch(fetchBooks(JSON.parse(booksFromLocalStorage).books));
     }
+    const categoriesFromLocalStorage = localStorage.getItem("myBookStore-categories");
+    if (categoriesFromLocalStorage) {
+      dispatch(fetchCategories(JSON.parse(categoriesFromLocalStorage).categories));
+    }
   }, []);
+
   return (
     <div className="background">
       <Router>
         <TopNavbar />
         <Switch>
           <Route exact path="/books" component={Books} />
+          <Route exact path="/categories" component={Categories} />
+
           <Route exact path="/add-book" component={AddBook} />
           <Route exact path="/add-category" component={AddCategory} />
         </Switch>
