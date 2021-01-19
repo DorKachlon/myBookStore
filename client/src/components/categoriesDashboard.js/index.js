25535093ab1e74509c8e7032bf2e9078b9b0e063
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2021-01-19 14:58:07 +0200

    emprty section for categories

--- client/src/components/categoriesDashboard.js/index.js
+++ client/src/components/categoriesDashboard.js/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./style.css";
 import { useSelector } from "react-redux";
 import CategoryCard from "../categoryCard";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import Tooltip from "@material-ui/core/Tooltip";
 
 export default function CategoriesDashboard() {
   const categories = useSelector((state) => state.categories);
@@ -9,9 +12,26 @@ export default function CategoriesDashboard() {
   const editing = useSelector((state) => state.editing);
   return (
     <div className="categories-dashboard">
-      {categories.map((category) => (
-        <CategoryCard key={category.id} category={category} deletion={deletion} editing={editing} />
-      ))}
+      {categories.length > 0 ? (
+        categories.map((category) => (
+          <CategoryCard
+            key={category.id}
+            category={category}
+            deletion={deletion}
+            editing={editing}
+          />
+        ))
+      ) : (
+        <div className="empty-section">
+          <h2>Categories list is Empty :(</h2>
+          <h2>Click here to add a new Category : </h2>
+          <Link to="/add-category">
+            <Tooltip title="Add a Category">
+              <Button variant="contained">add a Category</Button>
+            </Tooltip>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
