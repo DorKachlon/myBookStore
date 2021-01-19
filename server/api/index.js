ce55e4c53f30e2fcfda5b956c979372265004d86
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2021-01-19 15:10:37 +0200

    remove server folder

--- server/api/index.js
+++
@@ -1,5 +0,0 @@
-const api = require("express").Router();
-
-api.use("/v1", require("./v1"));
-
-module.exports = api;
--- server/api/v1/index.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-
-// router.use("/books", require("./books"));
-// router.use("/categories ", require("./categories "));
-
-module.exports = router;
